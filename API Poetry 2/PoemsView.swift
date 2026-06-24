bbbb4a866158747910fc5d05bdf7372cd268d2b3
Pam VanLeirsburg <pvanleirsburg@barrington220.org>
2026-06-24 17:12:07 -0500

    setup alert

---
+++ API Poetry 2/PoemsView.swift
@@ -0,0 +1,18 @@
+//
+//  PoemsView.swift
+//  API Poetry 2
+//
+//  Created by Pamela VanLeirsburg on 6/24/26.
+//
+
+import SwiftUI
+
+struct PoemsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PoemsView()
+}
