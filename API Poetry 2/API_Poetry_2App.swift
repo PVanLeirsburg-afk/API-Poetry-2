3f51e580bd611cc97fd4223247b2569a2059ec64
Pam VanLeirsburg <pvanleirsburg@barrington220.org>
2026-06-24 17:05:07 -0500

    Initial Commit

---
+++ API Poetry 2/API_Poetry_2App.swift
@@ -0,0 +1,17 @@
+//
+//  API_Poetry_2App.swift
+//  API Poetry 2
+//
+//  Created by Pamela VanLeirsburg on 6/24/26.
+//
+
+import SwiftUI
+
+@main
+struct API_Poetry_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
